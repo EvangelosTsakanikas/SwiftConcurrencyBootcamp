339c3885c33574ef8de4c77d9c242d9fced9244f
Evangelos Tsakanikas <evangelostsakanikas@gmail.com>
2025-02-03 01:38:49 +0100

    Added Actors Bootcamp

---
+++ SwiftConcurrencyBootcamp/ActorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ActorsBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by user274186 on 2/2/25.
+//
+
+import SwiftUI
+
+struct ActorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActorsBootcamp()
+}
